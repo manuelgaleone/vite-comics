fd8a6a66e2adbf3ad53afd6bfae01d1f6aec6fe0
manuelgaleone <galeonemanuel14@gmail.com>
2022-11-11 15:03:14 +0100

    Add component to the main one

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'Products List'
+}
+</script>
+
+<template>
+    <div class="grid">
+        <div class="col-2 card">
+            <div class="card_elements">
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                    alt="Card info">
+                <p class="card_title">
+                    ACTION COMICS
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
