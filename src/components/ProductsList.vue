288fd36206e2b69756b786a98bc648f2e5d16727
manuelgaleone <galeonemanuel14@gmail.com>
2022-11-11 15:24:04 +0100

    Working on cards style

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,21 +1,15 @@
 <script>
+import SingleProduct from '../components/SingleProduct.vue'
 export default {
-    name: 'Products List'
+    name: 'Products List',
+    components: {
+        SingleProduct
+    },
 }
 </script>
 
 <template>
-    <div class="grid">
-        <div class="col-2 card">
-            <div class="card_elements">
-                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
-                    alt="Card info">
-                <p class="card_title">
-                    ACTION COMICS
-                </p>
-            </div>
-        </div>
-    </div>
+    <SingleProduct></SingleProduct>
 </template>
 
 <style>
---
+++ src/components/SingleProduct.vue
@@ -0,0 +1,28 @@
+<script>
+import Products from '../assets/data/dc-comics.js'
+export default {
+    name: 'Single Product',
+    data() {
+        return {
+            products: Products
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="row align_items">
+        <div v-for="product in products" class="col-3 card">
+            <div class="card_elements">
+                <img :src="product.thumb" alt="Card info">
+                <p class="card_title">
+                    {{ product.series }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
